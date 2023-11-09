257a178ebad0c80bf84b18081666f3129c2dd7c3
jungkookswife <146013486+jungkookswife@users.noreply.github.com>
2023-11-09 19:37:30 +0500

    Генерация всех возможных вариантов рассадки сотрудников

--- sort.py
+++ sort.py
@@ -4,3 +4,18 @@ import itertools
 N = int(input("Введите количество сотрудников: "))
 distances = list(map(int, input("Введите расстояния от работы до домов сотрудников (через пробел): ").split()))
 tariffs = list(map(int, input("Введите тарифы за проезд одного километра в такси (через пробел): ").split()))
+
+min_cost = float('inf')
+min_taxi = []
+
+
+for permutation in itertools.permutations(range(N)):
+    total_cost = 0
+    taxi_arrangement = []
+
+    for i in range(N):
+        taxi = permutation[i]
+        cost = distances[i] * tariffs[taxi]
+        total_cost += cost
+        
+        taxi_arrangement.append(taxi + 1)
