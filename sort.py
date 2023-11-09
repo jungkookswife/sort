5dfded8cf78fe0742ac548fbf8126abb3dd4d1d9
jungkookswife <146013486+jungkookswife@users.noreply.github.com>
2023-11-09 19:40:06 +0500

    Создание функции для вывода суммы словами

--- sort.py
+++ sort.py
@@ -24,3 +24,24 @@ for permutation in itertools.permutations(range(N)):
     if total_cost < min_cost:
         min_cost = total_cost
         min_taxi = taxi_arrangement
+
+
+def convert_number_to_words(amount):
+    if amount < 1 or amount > 100000:
+        return "Сумма должна быть от 1 до 100 000"
+
+    words = num2words(amount, lang='ru')
+
+    last_digit = amount % 10
+    if last_digit == 1 and amount % 100 != 11:
+        currency = "рубль"
+    elif 2 <= last_digit <= 4 and (amount % 100 < 10 or amount % 100 >= 20):
+        currency = "рубля"
+    else:
+        currency = "рублей"
+
+    result = words.capitalize() + " " + currency
+
+    return result
+
+result = convert_number_to_words(min_cost)
