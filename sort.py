6ebf551cb3b16f8d11dead733e477623c5e25d3b
jungkookswife <146013486+jungkookswife@users.noreply.github.com>
2023-11-09 19:35:35 +0500

    Ввод данных пользователем

--- sort.py
+++ sort.py
@@ -1,2 +1,6 @@
 from num2words import num2words
 import itertools
+
+N = int(input("Введите количество сотрудников: "))
+distances = list(map(int, input("Введите расстояния от работы до домов сотрудников (через пробел): ").split()))
+tariffs = list(map(int, input("Введите тарифы за проезд одного километра в такси (через пробел): ").split()))
